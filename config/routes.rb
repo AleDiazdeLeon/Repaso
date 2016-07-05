f9d1c6cfe596dc86cd9241c34a8fb9bb246c8d2a
Alejandra Diaz de Leon <adiazdeleon@itesm.mx>
2016-07-04 20:56:04 -0500

    correcciones

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/pag1'
+
+  get 'pages/pag1'
   root 'pages#pag1'
-end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
